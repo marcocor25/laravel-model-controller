8646978d0f94346cb7a2128c1ec1b191e893ad14
Marco Corallo <94033528+marcocor25@users.noreply.github.com>
2022-04-21 15:56:33 +0200

    Caricamento database

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
 class MovieController extends Controller
 {
     public function index()
     {
+        $movies = Movie::all();
+        
         return view('layouts.standard');
     }
 }
