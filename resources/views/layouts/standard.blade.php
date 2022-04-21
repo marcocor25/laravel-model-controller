4e3c8619b777789eca227fe79811447dbe4c2747
Marco Corallo <94033528+marcocor25@users.noreply.github.com>
2022-04-21 15:34:32 +0200

    Caricamento route ecc

--- resources/views/layouts/standard.blade.php
+++ resources/views/layouts/standard.blade.php
@@ -5,11 +5,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <title>Movies</title>
 </head>
 
 <body>
-    
+
+    <h1>Test</h1>
+
 </body>
 
 </html>
