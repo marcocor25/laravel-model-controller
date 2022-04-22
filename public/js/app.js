ba6930786f2da4dd134a32ce5a93c6fcd66bed42
Marco Corallo <94033528+marcocor25@users.noreply.github.com>
2022-04-22 16:38:53 +0200

    .

--- public/js/app.js
+++ public/js/app.js
@@ -19432,8 +19432,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/marcocorallo/Desktop/Dev/LARAVEL/laravel-model-controller/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/marcocorallo/Desktop/Dev/LARAVEL/laravel-model-controller/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/marcocorallo/Dev/LARAVEL/laravel-model-controller/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/marcocorallo/Dev/LARAVEL/laravel-model-controller/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
